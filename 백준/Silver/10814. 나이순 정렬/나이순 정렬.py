8772a447fdb9f35fa751bc46780dc4fa47d8f829
leesoominida <81171305+leesoominida@users.noreply.github.com>
2022-10-23 20:11:43 +0900

    [Silver V] Title: 나이순 정렬, Time: 4248 ms, Memory: 45468 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+a = []
+for i in range(n):
+    age, name = map(str, input().split())
+    age = int(age)
+    a.append((age, name))
+    
+a.sort(key = lambda x: (x[0]))
+
+for i in a:
+    print(*i)
