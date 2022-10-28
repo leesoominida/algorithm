db498cd14f49cc78fbbaa9822a03393744a02447
leesoominida <81171305+leesoominida@users.noreply.github.com>
2022-10-28 21:11:51 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 108 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -0,0 +1,29 @@
+n, m = map(int, input().split())
+
+a = []
+cnt = []
+
+for  i in range(n):
+    a.append(input())
+    
+for i in range(n-7):
+    for j in range(m-7):
+        w = 0
+        b = 0
+        for k in range(i, i+8):
+            for l in range(j, j+8):
+                if (k+l)%2==0:
+                    if a[k][l]!='W':
+                        w += 1
+                    else:
+                        b +=1
+                else:
+                    if a[k][l] !='W':
+                        b += 1
+                    else:
+                        w += 1
+                        
+        cnt.append(w)
+        cnt.append(b)
+        
+print(min(cnt))
