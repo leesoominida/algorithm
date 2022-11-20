6a22beb2a9d45859923e234472b6e19ac046ab49
leesoominida <81171305+leesoominida@users.noreply.github.com>
2022-11-20 12:39:41 +0900

    [Silver IV] Title: 수 찾기, Time: 196 ms, Memory: 50092 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+A = set(map(int, input().split()))
+m = int(input())
+B = list(map(int, input().split()))
+
+for i in B:
+    if i in A:
+        print(1)
+    else:
+        print(0)
