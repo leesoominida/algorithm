2025db01d4e23e7badacee7010440eb00e86a3fe
leesoominida <81171305+leesoominida@users.noreply.github.com>
2022-11-23 09:15:12 +0900

    [Bronze III] Title: 전자레인지, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/10162. 전자레인지/전자레인지.py
@@ -0,0 +1,10 @@
+import sys
+T = int(sys.stdin.readline())
+A = B = C = 0
+if T % 10 != 0:
+    print(-1)
+else:
+    A = T // 300
+    B = (T % 300) //60
+    C = ((T % 300) % 60) // 10
+    print(A, B, C)
