e3e6fca66a80d597b1f289eec2d1e37f9338fec5
leesoominida <81171305+leesoominida@users.noreply.github.com>
2023-02-02 10:12:08 +0900

    [Silver III] Title: 1, Time: 124 ms, Memory: 14044 KB -BaekjoonHub

---
+++ 백준/Silver/4375. 1/1.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while(true){
+            String s = br.readLine( );
+            if(s == null){
+                break;
+            }
+            int n = Integer.parseInt(s);
+            int num = 0;
+            for (int i = 0;; i++) {
+                num = num*10 + 1;
+                num %= n;
+                if(num == 0){
+                    System.out.println(i+1);
+                    break;
+                }
+            }
+        }
+    }
+}
