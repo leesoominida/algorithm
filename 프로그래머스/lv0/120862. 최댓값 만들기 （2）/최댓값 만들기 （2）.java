27181206b9b08d5f9baac76aa4937558f753ef31
leesoominida <81171305+leesoominida@users.noreply.github.com>
2023-10-06 00:59:58 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.34 ms, Memory: 80.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        
+        Arrays.sort(numbers);
+        
+        int a = numbers[0] * numbers[1];
+        int b = numbers[numbers.length - 1] * numbers[numbers.length -2];
+        
+        
+        return a > b ? a : b;
+    }
+}
