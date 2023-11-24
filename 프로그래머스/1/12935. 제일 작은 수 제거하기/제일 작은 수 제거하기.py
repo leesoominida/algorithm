18a48bd36026a571fa29ef41a69c18a28eace061
leesoominida <81171305+leesoominida@users.noreply.github.com>
2023-11-24 22:05:03 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.py
@@ -0,0 +1,10 @@
+def solution(arr):
+    answer = []
+    
+    if len(arr) == 1:
+        answer.append(-1)
+    else:
+        answer = arr
+        answer.remove(min(arr))
+    
+    return answer
